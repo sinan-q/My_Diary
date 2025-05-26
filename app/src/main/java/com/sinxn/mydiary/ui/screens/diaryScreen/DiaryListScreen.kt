241ddd55d9943aa23aaa6081a514685f749fd857
Sinan Muhammed <sinan9539391118@gmail.com>
2025-05-26 22:23:44 +0530

    Show message when no diary exists

--- app/src/main/java/com/sinxn/mydiary/ui/screens/diaryScreen/DiaryListScreen.kt
+++ app/src/main/java/com/sinxn/mydiary/ui/screens/diaryScreen/DiaryListScreen.kt
@@ -1,17 +1,23 @@
 package com.sinxn.mydiary.ui.screens.diaryScreen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.sinxn.mydiary.ui.components.MyTopAppBar
 import com.sinxn.mydiary.ui.components.RectangleFAB
@@ -34,13 +40,18 @@ fun DiaryListScreen(
         topBar = {
             MyTopAppBar(onBackup)
         },
-        modifier = modifier.fillMaxSize()
     ) { paddingValues ->
+        if(diaries.isEmpty())
+            Box(modifier = Modifier.fillMaxSize().padding(paddingValues), contentAlignment = Alignment.Center) {
+                Text("No Diaries Found", fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+            }
+
         LazyColumn(
             contentPadding = paddingValues,
             verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier
+            modifier = Modifier.fillMaxSize()
         ) {
+
             items(diaries) { diary ->
                 DiaryItem(
                     diary = diary,

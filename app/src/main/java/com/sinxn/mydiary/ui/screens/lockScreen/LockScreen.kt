956dfac94fc41cdca6056a0aef498b4a82e75d96
Sinan Muhammed <sinan9539391118@gmail.com>
2025-05-27 21:45:27 +0530

    Black theme in LockScreen

--- app/src/main/java/com/sinxn/mydiary/ui/screens/lockScreen/LockScreen.kt
+++ app/src/main/java/com/sinxn/mydiary/ui/screens/lockScreen/LockScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,6 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sinxn.mydiary.R
+import com.sinxn.mydiary.ui.theme.MyDiaryTheme
 import com.sinxn.mytasks.ui.components.RectangleButton
 import showBiometricsAuthentication
 
@@ -38,27 +40,33 @@ fun LockScreen(
             }
         )
     }
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            HorizontalDivider()
-            Text(text = stringResource(R.string.app_name), fontSize = 24.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(vertical = 10.dp))
-            HorizontalDivider()
-            Spacer(modifier = Modifier.padding(bottom = 30.dp))
-            Text(text = stringResource(R.string.auth_locked))
-            Spacer(modifier = Modifier.padding(bottom = 10.dp))
-            RectangleButton(onClick = {
-                authenticate(onUnlock)
-            }) {
-                Text( text = stringResource(R.string.auth_locked_button), textAlign = TextAlign.Center)
+    MyDiaryTheme {
+        Scaffold {
+            Box(modifier = Modifier.fillMaxSize().padding(it), contentAlignment = Alignment.Center) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    HorizontalDivider()
+                    Text(text = stringResource(R.string.app_name), fontSize = 24.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(vertical = 10.dp))
+                    HorizontalDivider()
+                    Spacer(modifier = Modifier.padding(bottom = 30.dp))
+                    Text(text = stringResource(R.string.auth_locked))
+                    Spacer(modifier = Modifier.padding(bottom = 10.dp))
+                    RectangleButton(onClick = {
+                        authenticate(onUnlock)
+                    }) {
+                        Text( text = stringResource(R.string.auth_locked_button), textAlign = TextAlign.Center)
+                    }
+                    //TODO Remove this button
+//                    RectangleButton(onClick = {
+//                        onUnlock()
+//                    }) {
+//                        Text( text = stringResource(R.string.auth_locked_button), textAlign = TextAlign.Center)
+//                    }
+                }
             }
-            //TODO Remove this button
-//            RectangleButton(onClick = {
-//                onUnlock()
-//            }) {
-//                Text( text = stringResource(R.string.auth_locked_button), textAlign = TextAlign.Center)
-//            }
         }
+
     }
+
     LaunchedEffect(Unit) {
         authenticate(onUnlock)
     }

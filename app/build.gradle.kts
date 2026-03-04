a53f3f40399325c3652396be564e01e8030c242e
Sinan Muhammed <sinan9539391118@gmail.com>
2026-03-04 23:15:00 +0530

    update version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.sinxn.mydiary"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

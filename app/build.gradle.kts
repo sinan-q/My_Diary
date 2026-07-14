3174107b9501ec9bc46136048967dc1b4fa5d5c1
Sinan Muhammed <sinan9539391118@gmail.com>
2026-07-14 21:25:38 +0530

    minSdk to 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sinxn.mydiary"
-        minSdk = 31
+        minSdk = 29
         targetSdk = 36
         versionCode = 3
         versionName = "1.0.2"
--- app/src/main/java/com/sinxn/mydiary/ui/theme/Theme.kt
+++ app/src/main/java/com/sinxn/mydiary/ui/theme/Theme.kt
@@ -39,7 +39,7 @@ fun MyDiaryTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor -> {
+        dynamicColor && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
